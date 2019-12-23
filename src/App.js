baf9ae66b8f9da87b847c81e0af0ea23046d14a6
soldered-snake <kyle.vinc11@gmail.com>
2019-12-23 11:51:55 +1000

    Installed Dependencies
    Installed base dependancies and got the basic functionallity for firebase working.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebase = require('firebase');
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIntex: null,
+      selectedNote: null,
+      notes: null
+    }
+  }
+
+  render(){
+    return(
+        <div> Hello world!</div>
+    )
+  }
+
+  //Upon successful loading of component
+  componentDidMount = () => {
+    //On snapshot (update) of the notes collection... do x function
+    firebase.firestore().collection('notes').onSnapshot(serverUpdate => {
+      //Notes array populated with map,
+      const notes = serverUpdate.docs.map(doc => {
+        //data is the data for each doc in the collection
+        const data = doc.data();
+        //Creating a new field on 'data' called 'id' and assigning it the doc id.
+        data['id'] = doc.id;
+        return data;
+      });
+      console.log(notes);
+      this.setState({notes: notes})
+    });
+  }
+
 }
 
 export default App;
